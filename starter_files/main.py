00f12d493b6449f775b896c75ce49a1affdf7ef4
Mariya <mariyasha432@protonmail.com>
2024-08-19 08:46:40 -0700

    uploading starter files

---
+++ starter_files/main.py
@@ -0,0 +1,10 @@
+import taipy as tp
+import taipy.gui.builder as tgb
+
+# create page
+with tgb.Page() as page:
+    tgb.text("my beautiful application goes here!")
+
+if __name__ == "__main__":
+    gui = tp.Gui(page)
+    gui.run(title = "Data Sceince Dashboard")
